7b5eec6af3bc79b97faf494c61cc0b3645f6ab53
I love my self❤️💙 <anjayalfan@gmail.com>
2024-11-05 02:26:24 +0700

    Delete tugas 4.py

--- tugas 4.py
+++
@@ -1,7 +0,0 @@
-print ("Nama saya Muhammad Alfan Hidayatullah")
-print ("Nim saya adalah 24241190")
-
-print ("Saya berasal dari kota Mataram")
-
-print ("Nim pertama saya 24 + Nim terakhir saya 90")
-print ("Jumlah dari dua digit pertama dan terakhir adalah 114")
