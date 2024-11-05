ec97e971f04cda0bfa6b9d72f19ec4d442d8e023
I love my self❤️💙 <anjayalfan@gmail.com>
2024-11-05 10:14:45 +0700

    Add files via upload

---
+++ tugas 4.py
@@ -0,0 +1,13 @@
+# Tugas 4 by Muhammad Alfan
+
+name = "Muhammad Alfan Hidayatullah"
+address = "Mataram"
+nim = "24241190"
+
+firstNim = 24
+lastNim = 90
+
+result = firstNim + lastNim
+
+print(f" Nama saya adalah {name}, Saya berasal dari {address}, nim saya {nim}")
+print(f" hasil penjumlahan dari {firstNim} + {lastNim} = {result} ")
