59a05230fe996558a8053837a962297fafd56035
I love my self❤️💙 <anjayalfan@gmail.com>
2024-11-05 10:15:06 +0700

    Add files via upload

---
+++ tugas 5.py
@@ -0,0 +1,12 @@
+# Tugas 5 by Muhammad Alfan Hidayatulllah
+
+nim = "24241190"
+
+nimAwal = 24
+nimAkhir = 90
+
+panjang  = nimAkhir
+lebar  = nimAwal
+
+luas = panjang * lebar
+print(f"luas dari {panjang} x {lebar} adalah = {luas}")
