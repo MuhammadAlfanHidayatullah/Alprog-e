701328450ac69d84e0b3220dda92db7edbe1454f
I love my self❤️💙 <anjayalfan@gmail.com>
2024-11-19 04:45:19 +0700

    Add files via upload

---
+++ tugas 7.py
@@ -0,0 +1,13 @@
+# Tugas 7 by Muhammad Alfan Hidayatullah
+
+# nama  : Muhammad Alfan Hidayatullah
+# Nim   : 24241190
+                                #"Menghitung Persegi Panjang"#
+
+print("persegi panjang")
+
+panjang = float(input("masukan panjang :"))
+lebar = float(input("masukkan lebar :"))
+
+hasil = panjang * lebar
+print(f"Hasil dari {panjang} x {lebar} adalah : {hasil}") 
