1988b605661bba45df52db78ce4aa41880e26897
I love my self❤️💙 <anjayalfan@gmail.com>
2024-11-29 09:44:22 +0800

    Add files via upload

---
+++ tugas 8.py
@@ -0,0 +1,29 @@
+              #aritmatika
+x = 24
+y = 90
+#penjumlahan
+hasil = x + y
+print ("hasil dari x = y")
+#pengurangan
+hasil = x - y
+print ("nilai dari x - y = ", hasil)
+
+#pembagian
+hasil = x / y
+print ("hasil dari x / y = ", hasil)
+
+#perkalian
+hasil = x * y
+print ("hasil dari x * y =", hasil)
+
+#exponen (perpangkatan)
+hasil = x ** y
+print ("nilai dari x eksponen y =", hasil)
+
+#Modulus (sisa bagi)
+hasil = x % y
+print ("hasil dari x mod y =", hasil)
+
+#floor division
+hasil = x // y
+print ("hasil dari x FD y =", hasil)
