f45577392475c77c1ad9a3fdcb04a2a8adeb48e0
I love my self❤️💙 <anjayalfan@gmail.com>
2024-12-08 11:09:23 +0800

    Update tugas 9.py

--- tugas 9.py
+++ tugas 9.py
@@ -5,7 +5,7 @@ nilai = float(input("masukkan nilai suhu dalam celcius :"))
 fahrenheit = (nilai * 9/5)+32
 print("nilai dari fahrenheit = ", fahrenheit)
 
-kelvin = nilai + 273,15
+kelvin = (nilai + 273,15)
 print("nilai dari kelvin = ", kelvin)
 
 reamur = (nilai * (5/4)+0)
