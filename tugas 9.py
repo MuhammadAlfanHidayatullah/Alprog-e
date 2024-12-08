67f218216aa817644489ed7e9b2877ce8d5dd296
I love my self❤️💙 <anjayalfan@gmail.com>
2024-12-08 18:52:56 +0700

    Add files via upload

---
+++ tugas 9.py
@@ -0,0 +1,13 @@
+# oprasi aritmatika
+# menghitung celcius
+
+suhu = float(input("Masukkan suhu celcius = "))
+
+fahrentheit = (suhu * 9/5) + 32
+print(f"{suhu} celcius = {fahrentheit} fahrenheit")
+
+kelvin = suhu + 273,15
+print(f"{suhu} celcius = {kelvin} kelvin")
+
+reamur = suhu * (4/5)
+print(f"{suhu} celcius = {reamur} (reamur)")
